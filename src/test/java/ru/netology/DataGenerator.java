fa5946d0b10773d3fdb247bae9731616d095f5d6
Илья <ila@iMac-Ila.local>
2023-04-28 18:48:51 +0300

    first commit

---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static Faker faker = new Faker(new Locale("ru"));
+
+    public static String generateDate(int shift) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate date = LocalDate.now();
+        return date.plusDays(shift).format(dateFormat);
+    }
+
+    public static String generateCity(String locale) {
+        String city = faker.address().city();
+        return city;
+    }
+
+    public static String generateName(String locale) {
+        String name = faker.name().lastName() + " " + faker.name().firstName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            UserInfo user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -0,0 +1,57 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryTest {
+
+    @BeforeEach
+    public void setUpTest() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldChangeDateTest() {
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        val daysToAddForFirstMeeting = 4;
+        val firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        val daysToAddForSecondMeeting = 7;
+        val secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $("[placeholder='Город']").setValue(validUser.getCity());
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
+        $("[name='name']").setValue(validUser.getName());
+        $("[name='phone']").setValue(validUser.getPhone());
+        $(".checkbox__box").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+
+        if ($(withText("Доставка в выбранный город недоступна")).isDisplayed()) {
+            int i = validUser.getCity().length() - 2;
+            while (i > 0) {
+                $("[data-test-id=city] .input__control").sendKeys(Keys.BACK_SPACE);
+                i--;
+            }
+            $(".menu-item__control").click();
+            $(".button__text").click();
+        }
+
+        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(secondMeetingDate);
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("div.notification__content > button").click();
+        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
